587c3800f7fc91f9f22e83a9a0c17a115dbf2961
Moreno-ID <50474363+Moreno-ID@users.noreply.github.com>
2019-05-14 10:17:56 +0000

    Add files via upload

---
+++ Wifi.py
@@ -0,0 +1,9 @@
+clear
+
+figlet HACK-WIFI | lolcat
+echo "-------------------------------" | lolcat
+echo "Author   : PS1K0P4T           |" | lolcat
+echo "Team     : B0C4H-C0D1N6       |" | lolcat
+echo "FB       : Moreno ID          |" | lolcat
+echo "Thanks To: Allah S.W.T        |" | lolcat
+echo "-------------------------------" | lolcat
---
+++ Wifi.sh
@@ -0,0 +1,37 @@
+#MauRecode?
+#HhDasarBocahMainRecode
+#BuatSendiriDong
+clear
+figlet "HACK-WIFI" | lolcat
+echo "--------------------------------" | lolcat
+echo "|Author   : PS1K0P4T           |" | lolcat
+echo "|Team     : B0C4H-C0D1N6       |" | lolcat
+echo "|FB       : Moreno ID          |" | lolcat
+echo "|Thanks To: Allah S.W.T        |" | lolcat
+echo "|Github   : Moreno-ID          |" | lolcat
+echo "--------------------------------" | lolcat
+echo ""
+echo "Silahkan Pilih Target" | lolcat
+echo "1.Wifi ID
+2.Coffe 45
+3.Modal Lu
+4.Caffe Arjuna
+9.Keluar"
+read -p "Pilihan Kamu : " pil;
+sleep 5
+echo "Kamu Memilih $pil" | lolcat
+sleep 3
+echo "Menganalisa $pil" | lolcat
+sleep 3
+echo "Mulai Menghubungkan $pil" | lolcat
+sleep 3
+echo "$pil Berhasil DiHack" | lolcat
+echo "Result
+Nama Wifi : $pil
+IP : 189 731 5211
+Keamanan : Wpa2
+Password : Indonesia54"
+
+echo ""
+read -p "Mau Cari Lagi? :" next;
+
