00961b5bc1a05dcc42533bcc4780ed4b177232d0
Moreno-ID <50474363+Moreno-ID@users.noreply.github.com>
2019-05-18 12:44:07 +0000

    Update Wifi.sh

--- Wifi.sh
+++ Wifi.sh
@@ -4,8 +4,8 @@
 clear
 figlet "HACK-WIFI" | lolcat
 echo "--------------------------------" | lolcat
-echo "|Author   : PS1K0P4T           |" | lolcat
-echo "|Team     : B0C4H-C0D1N6       |" | lolcat
+echo "|Author   : GUS10N             |" | lolcat
+echo "|Team     : NoTeam[NeedTeam]       |" | lolcat
 echo "|FB       : Moreno ID          |" | lolcat
 echo "|Thanks To: Allah S.W.T        |" | lolcat
 echo "|Github   : Moreno-ID          |" | lolcat
